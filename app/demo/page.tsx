b106ad4a0a8d7aafec5b6e89307d0d35413a8f14
karmCS <157756639+karmCS@users.noreply.github.com>
2026-04-02 23:07:51 -0700

    Added page.tsx

---
+++ app/demo/page.tsx
@@ -0,0 +1,9 @@
+import NavMenu from "@/components/ui/menu-hover-effects";
+
+export default function DemoPage() {
+  return (
+    <div className="flex w-full h-screen justify-center items-center bg-white">
+      <NavMenu />
+    </div>
+  );
+}
