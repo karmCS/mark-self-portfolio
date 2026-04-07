fe7f0df4eb06c55cce99fe8baf8bfd09e0ee822b
karmCS <157756639+karmCS@users.noreply.github.com>
2026-04-06 19:16:08 -0700

    Created Dockerfile for live deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
-};
+  output: 'standalone',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
