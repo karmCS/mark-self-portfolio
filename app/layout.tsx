5ad9bf35d37174437c3ce14662a9ba37f49ba2de
karmCS <157756639+karmCS@users.noreply.github.com>
2026-04-08 00:43:41 -0700

    Fixed: metadata showing bolt snapshot

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,21 +11,6 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: 'Personal Portfolio',
   description: 'Computer Engineering Student Portfolio',
-  openGraph: {
-    images: [
-      {
-        url: 'https://bolt.new/static/og_default.png',
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    images: [
-      {
-        url: 'https://bolt.new/static/og_default.png',
-      },
-    ],
-  },
 };
 
 export default function RootLayout({
